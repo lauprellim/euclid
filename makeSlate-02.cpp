54fa41c3ee1541540c2d57d52c16ef3d973fccc3
lauprellim <86619288+lauprellim@users.noreply.github.com>
2024-08-03 21:52:43 -0400

    Add files via upload

---
+++ makeSlate-02.cpp
@@ -0,0 +1,116 @@
+/*
+Version 2
+Works
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std; 
+
+// boldface
+static const string boldOn = "\e[1m";
+static const string boldOff = "\e[0m";
+
+class Slate {
+    private:
+        int a = 1;
+        int iter = 0;
+        int counter = 1;
+        int depth = 2;
+
+    public:
+        int n, kn;
+        vector<vector<int>> NKSlate;
+    
+    Slate ( int n=12, int kn=5, int depth=2 ) {
+        this->n = n;
+        this->kn = kn;
+        this->depth = depth;
+    }
+
+    void setN(int n) { this->n = n; }
+    void setKn(int kn) { this->kn = kn; }
+    void setDepth(int depth) { this->depth = depth; }
+
+    int getN() { return n; }
+    int getKn() { return kn; }
+    int getDepth() { return depth; }
+
+    // make a matrix ("slate")
+    // NKSlate = makeSlate(a, iter, depth, NK, NKSlate );
+
+    // member function makeNK
+    void makeSlate() {
+        // first make the NK array
+        depth--;
+        int NK[depth+2];
+
+        // insert middle values in array as starting values
+        NK[0] = n;
+        NK[depth+1] = kn;
+        for(int i=1; i<=depth; i++) { NK[i] = n-i; }
+        // then call the makeSlate member function
+        NKSlate = makeSlate( a, iter, depth, NK, NKSlate );
+    }
+
+    // member function makeSlate
+    vector<vector<int>> makeSlate( int a, int iter, int depth, int* NK, vector<vector<int>> NKSlate ) {
+        // terminal condition
+        if(a == depth+1) { return NKSlate; }
+
+        for(NK[a]=NK[a-1]-1; NK[a]>=NK[depth+1]+(depth-iter); NK[a]--) {
+            // check to see if the deepest loop has been reached.
+            // only then print out the variables
+            if(a == depth) {
+                // fill a row of the 2D vector first
+                vector<int> NKSlateThisRow;
+                for(int i=0; i<depth+2; i++) {
+                    NKSlateThisRow.push_back(NK[i]);
+                }
+                // now push the row onto the 2D vector
+                NKSlate.push_back(NKSlateThisRow);
+                counter++;
+            }
+            NKSlate = makeSlate( a+1, iter+1, depth, NK, NKSlate );
+        }
+        return NKSlate;
+    }
+
+    // member function printSlate
+    void printSlate() {
+        for(int i=0; i<NKSlate.size(); i++) {
+            cout<<"Row "<<i<<":\t";
+            for(int j=0; j<NKSlate[i].size(); j++) {
+                cout<<NKSlate[i][j]<<"\t";
+            }
+            cout<<endl;
+        }
+    }
+};
+
+int main() { 
+    // crucial to set the depth here.
+    // depth must be greater than 0, and no greater than 
+    // the difference of n and kn (???)
+
+    int depth, n, kn;
+
+    cout<<boldOn<<"MAKE MANY HYPEREUCLIDEAN SETS"<<boldOff<<endl;
+    cout<<"What is your desired depth? (1-5): ";
+    cin>>depth;
+
+    cout<<"What is your desired starting span (n)? ";
+    cin>>n;
+
+    cout<<"What is your desired ending span (kn)? ";
+    cin>>kn;
+
+    Slate mySlate;
+    mySlate.setN(n);
+    mySlate.setKn(kn);
+    mySlate.setDepth(depth);
+    mySlate.makeSlate();
+    mySlate.printSlate();
+
+    return 0;
+}
